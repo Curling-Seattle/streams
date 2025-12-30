0584f5b0bbcac23d159a450949443b98f02d5bfd
John Canning <JMCanning@alum.mit.edu>
2025-12-30 15:17:05 -0800

    Add process exit monitoring after launching OBS

--- start-streams.ps1
+++ start-streams.ps1
@@ -2,6 +2,8 @@
 # a composite view of a curling sheet.
 
 $dbg = $false
+$initialDelay = 30
+$pollEvery = 60
 $localConfig = "$PSScriptRoot\start-streams-config.ps1"
 if (-not (Test-Path $localConfig)) {
     Write-Host "No $localConfig file found"
@@ -10,17 +12,19 @@ if (-not (Test-Path $localConfig)) {
     . $localConfig
 }
 
+Write-Host "Starting OBS process initialization script ..."
+
 # First, verify that YouTube is reachable over the network
 while (-not (Test-Connection -ComputerName youtube.com -Count 1)) {
-    Write-Host "Network disconnected. Waiting..."
+    Write-Host "No network connection to youtube. Waiting..."
     Start-Sleep -Seconds 5 # Wait for 5 seconds before checking again
 }
+Write-Host "Connection to youtube works."
 
-# Wait for 30 seconds to make sure everything has started up
-if ($dbg) {
-    Write-Host "Skip initial sleep period"
-} else {
-    Start-Sleep -Seconds 30
+# Wait for a certain period to make sure everything has started up
+if ($initialDelay -gt 0) {
+    Write-Host "Sleeping $initialDelay seconds at start up"
+    Start-Sleep -Seconds $initialDelay
 }
 
 function Notify-GCC {
@@ -31,50 +35,70 @@ function Notify-GCC {
 	Write-Host Invoke-RestMethod -Uri `$webhookURL -Method Post `
 	  -Body $payload -ContentType "application/json"
     } else {
+	Write-Host "Sending '$msg' to web-cast channel"
 	$payload = @{text = $msg} | ConvertTo-Json
 	Invoke-RestMethod -Uri $webhookURL -Method Post `
 	  -Body $payload -ContentType "application/json"
     }
 }
 
-# On each stream server, select a range of sheets to monitor
-$first_sheet = 4
-$last_sheet = 5
+# On each stream server, select which sheets to monitor
+$sheets = 4, 5
+
+# Associative array to may process IDs to sheet numbers
+$sheet_from_pid = @{}
 
 # Build a list of OBS processes that create the composite view for the streams
-$proc_list = [System.Collections.Generic.List[int]]::new()
-for ($i = $first_sheet; $i -le $last_sheet; $i++) {
-    Write-Host Set-Location "C:\GCC-Streaming\obs-studio-shee-$i\bin\64bit\"
-    Write-Host "`$proc_list.Add(StartProcess `
-      -FilePath `"C:\GCC-Streaming\obs-studio-sheet-$i\bin\64bit\obs64.exe`" `
-      -ArgumentList `"-m --startstreaming`" -PassThru)"
-    Write-Host "Register-ObjectEvent -InputObject `$proc_list[-1] `
-      -EventName Exited -Action {Notify-GCC(`"Sheet $i OBS process died`")} "
+$proc_list = [System.Collections.Generic.List[System.Diagnostics.Process]]::new()
+foreach ($i in $sheets) {
+    $sheetPath = "C:\GCC-Streaming\obs-studio-sheet-$i\bin\64bit"
+    $exePath = "$sheetPath\obs64.exe"
+    $running = (Get-Process | Where-Object { $_.Path -eq $exePath }).Count -gt 0
+    if ($running) {
+	Write-Host "Found process running in $exePath. Skipping OBS launch."
+	Start-Sleep -Seconds 2
+    } else {
+	Write-Host "Starting OBS process for sheet $i ..."
+	if ($dbg) {
+	    $proc = Start-Process `
+	      "/Applications/Emacs.app/Contents/MacOS/Emacs" `
+	      -ArgumentList "$($i)away-horiz.png" -PassThru
+	} else {
+	    Set-Location $sheetPath
+	    $proc = Start-Process -FilePath $exePath `
+	      -ArgumentList "-m --startstreaming" -PassThru
+	}
+	$proc_list.Add($proc)
+	$sheet_from_pid[$proc.Id] = $i
+	Start-Sleep -Seconds 5
+    }
 }
 
-# Keep this script going until all processes have exited
-$proc_list = @("proc1", "proc2")
-$alive = $false
-$count = 0
-do {
-    $alive = $false
-    foreach ($proc in $proc_list) {
-	if ($dbg) {
-            Write-Host "if (-not `$$proc.HasExited) { `
-	      `$alive = `$true `
-	      } "
-	} else {
+Set-Location $PSScriptRoot
+
+if ($proc_list.Count -gt 0) {
+    # Keep this script going until all processes have exited
+    Write-Host "Waiting until OBS processes exit..."
+    do {
+	$alive = $false
+	foreach ($proc in $proc_list) {
 	    if (-not $proc.HasExited) {
 		$alive = $true 
+	    } elseif ($sheet_from_pid[$proc.Id] -gt 0) {
+		Notify-GCC("Process died for sheet $($sheet_from_pid[$proc.Id]) (id $($proc.Id))")
+		$sheet_from_pid[$proc.Id] = 0
+	    } elseif ($dbg) {
+		Write-Host "Process ID $($proc.Id) died and sheet_from_pid = $($sheet_from_pid[$proc.Id])"
 	    }
 	}
-    }
-    if ($dbg) {
-	Write-Host "Start-Sleep -Seconds 60"
-	$alive = $count++ -lt 3
-    } else {
-	Start-Sleep -Seconds 60
-    }
-} while ($alive)
+	if ($dbg) {
+	    Write-Host "Start-Sleep -Seconds $pollEvery"
+	}
+	Start-Sleep -Seconds $pollEvery
+    } while ($alive)
 
-Notify-GCC("All OBS processes for sheets $first_sheet to $last_sheet have exited.")
+    Notify-GCC("All OBS processes for sheets " + ($sheets -join ', ') +
+	       " have either exited or were launched by another script.")
+} else {
+    Write-Host "No processes launched."
+}
